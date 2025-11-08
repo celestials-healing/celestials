79b0f933f2e9ae79e30874164b541e87e98333bb
Durgesh Tiwari <twdurgesh226@gmail.com>
2025-11-08 14:22:43 +0530

    landing apge

--- app/components/video3.tsx
+++ app/components/video3.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import { Play } from 'lucide-react';
 
 // Video player component that supports YouTube and Google Drive links
